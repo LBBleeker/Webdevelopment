0cc4ecf54ae92f173e2769c8eeacaf6e4c3fda23
LBBleeker <luuk.b.bleeker@gmail.com>
2022-04-04 16:51:03 +0200

    fixed!!!!

--- final_project_LBBleeker/script.js
+++ final_project_LBBleeker/script.js
@@ -1,23 +1,4 @@
-// const colors = {
-//   red: ["#ff0000"],
-//   blue: ["#0000ff"],
-//   yellow: ["#ffff00"],
-//   green: ["00ff00"],
-//   black: ["#000000"],
-//   white: ["#ffffff"]
-// };
-
-// const tl = new TimelineLite();
-
-// function radioListener() {
-// 	const colorLink = document.querySelector('.menu-toggle');
-// colorLink.addEventListener("click", function(e) {
-// 	const value = colorLink.querySelector('input[name="colors"]:checked').id;
-// 	update(value)
-// });
-
-// function setColors(){
-//   const colorSelector = document.querySelector(".color-selector");
-//   Object.keys(colors).map((key, index) => {
-//     colorSelector.style.setProperty(`--${key}`, colors[key][0]);
-//   });
+const bgchange = (id) => {
+	document.body.style.background = document.getElementById(id).innerHTML;
+  document.getElementById("checkbox").checked = false;
+};
