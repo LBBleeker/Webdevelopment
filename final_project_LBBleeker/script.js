2506e452b426da281b3dbb3e922ffe37958a55b4
LBBleeker <luuk.b.bleeker@gmail.com>
2022-04-04 16:51:23 +0200

    fixed

--- final_project_LBBleeker/script.js
+++ final_project_LBBleeker/script.js
@@ -1,4 +1,4 @@
 const bgchange = (id) => {
 	document.body.style.background = document.getElementById(id).innerHTML;
-  document.getElementById("checkbox").checked = false;
+	document.getElementById("checkbox").checked = false;
 };
